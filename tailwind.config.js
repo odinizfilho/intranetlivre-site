f7ae5997c921b2729040c92b92602c87c6a50839
Gabriel Diniz <bonjornogabriel@gmail.com>
2024-04-20 14:13:06 -0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+        fontFamily: {
+            sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        },
+    },
+},
+plugins: [
+  forms,
+  typography,
+],
 }
 
